6c5fadc0e0e2a1a337d0e6d8ecaf206f8bbd1256
SigmaFong <ahfong2548@gmail.com>
2024-04-23 20:49:16 +0700

    Add delete reservation after confirm reservation

--- backend/src/controllers/reservation.ts
+++ backend/src/controllers/reservation.ts
@@ -189,6 +189,7 @@ export async function confirmReservation(req: Request,res: Response,next: NextFu
             await reserver.save({ session });
             await session.commitTransaction();
             session.endSession();
+            await ReservationModel.findByIdAndDelete(reservation._id);
             return res.status(200).json({
                 success: true,
                 data: reservation
