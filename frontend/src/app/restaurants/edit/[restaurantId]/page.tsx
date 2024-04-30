0755d4b73d7160f6a13064939a0889b4519b7573
NewBieCoderXD <fruity5555@gmail.com>
2024-04-30 16:11:08 +0700

    fix restaurant image url; put token in edit restaurant page

--- frontend/src/app/restaurants/edit/[restaurantId]/page.tsx
+++ frontend/src/app/restaurants/edit/[restaurantId]/page.tsx
@@ -1,6 +1,7 @@
 "use server";
 import { Discount, RestaurantResponse } from "@/../interface";
 import EditRestaurantForm from "@/components/EditRestaurantForm";
+import useServerSession from "@/hooks/useServerSession";
 import getRestaurant from "@/utils/getRestaurant";
 
 export default async function ({
@@ -10,9 +11,12 @@ export default async function ({
     restaurantId: string;
   };
 }) {
+  const session = await  useServerSession();
   let restaurantInformation: RestaurantResponse = await getRestaurant(
     params.restaurantId + "?time=" + new Date(),
+    session?.token
   );
+  console.log(restaurantInformation)
   restaurantInformation.data.discounts.forEach((discount) => {
     discount.canEdit = discount.isValid;
   });
--- frontend/src/utils/getClientRestaurantImageUrl.ts
+++ frontend/src/utils/getClientRestaurantImageUrl.ts
@@ -1,6 +1,6 @@
 "use client";
 export default function getRestaurantImageData(restaurantId: string) {
-  const origin = typeof window !== 'undefined' && (window.location.origin ?? '');
+  const origin = typeof window !== 'undefined' ? (window.location.origin) : "http://localhost:3000";
   let queryParams = new URLSearchParams();
   queryParams.append("time", new Date().getTime().toString());
   return `${origin}/api/restaurants/${restaurantId}/image/?${queryParams.toString()}`;
